d801abe1d8f87d14b3dc1b5fae84eff3c9fb5586
ya158 <koros@ya.ru>
2025-03-19 17:21:56 +0300

    Create All.sh

---
+++ All.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget --no-check-certificate -O /tmp/Terminal.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/Terminal.sh && chmod +x /tmp/Terminal.sh && /tmp/Terminal.sh
+rm -f /tmp/Terminal.sh
+wget --no-check-certificate -O /tmp/led.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/led.sh && chmod +x /tmp/led.sh && /tmp/led.sh
+rm -f /tmp/led.sh
+wget --no-check-certificate -O /tmp/NetBird.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/NetBird && chmod +x /tmp/NetBird.sh && /tmp/NetBird.sh
+rm -f /tmp/led.sh
