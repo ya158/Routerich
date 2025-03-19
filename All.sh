0c925cf4aef072e4b9f3436473cdc6b01fb2709e
ya158 <koros@ya.ru>
2025-03-19 21:42:41 +0300

    Update All.sh

--- All.sh
+++ All.sh
@@ -4,5 +4,5 @@ wget --no-check-certificate -O /tmp/Terminal.sh https://raw.githubusercontent.co
 rm -f /tmp/Terminal.sh
 wget --no-check-certificate -O /tmp/led.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/led.sh && chmod +x /tmp/led.sh && /tmp/led.sh
 rm -f /tmp/led.sh
-wget --no-check-certificate -O /tmp/NetBird.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/NetBird && chmod +x /tmp/NetBird.sh && /tmp/NetBird.sh
-rm -f /tmp/led.sh
+wget --no-check-certificate -O /tmp/NetBird.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/NetBird.sh && chmod +x /tmp/NetBird.sh && /tmp/NetBird.sh
+rm -f /tmp/NetBird.sh
