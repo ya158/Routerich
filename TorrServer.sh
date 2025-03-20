90d47382fa175d3882d33387d21278629a416b21
ya158 <koros@ya.ru>
2025-03-20 14:30:12 +0300

    Create TorrServer.sh

---
+++ TorrServer.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget  --no-check-certificate -O /etc/torrserv/TorrServer-linux-arm64 https://github.com/YouROK/TorrServer/releases/download/MatriX.134/TorrServer-linux-arm64 && chmod +x /etc/torrserv/TorrServer-linux-arm64
+opkg update
+opkg install upx
+upx --lzma --best /etc/torrserv/TorrServer-linux-arm64
+sed -i -e '(/etc/torrserv/TorrServer-linux-arm64 -p 8090 >/dev/null 2>&1 ) &' /etc/rc.local
