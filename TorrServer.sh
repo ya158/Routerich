fe381004ffc240913b7437b3316964cda22aadcf
ya158 <koros@ya.ru>
2025-03-20 14:38:24 +0300

    Update TorrServer.sh

--- TorrServer.sh
+++ TorrServer.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir -p /etc/torrserv
 wget  --no-check-certificate -O /etc/torrserv/TorrServer-linux-arm64 https://github.com/YouROK/TorrServer/releases/download/MatriX.134/TorrServer-linux-arm64 && chmod +x /etc/torrserv/TorrServer-linux-arm64
 opkg update
 opkg install upx
