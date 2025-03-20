a21207151bdbe8449dd28f0811a84d13c79e5654
ya158 <koros@ya.ru>
2025-03-20 14:57:21 +0300

    Update TorrServer.sh

--- TorrServer.sh
+++ TorrServer.sh
@@ -5,4 +5,4 @@ wget  --no-check-certificate -O /etc/torrserv/TorrServer-linux-arm64 https://git
 opkg update
 opkg install upx
 upx --lzma --best /etc/torrserv/TorrServer-linux-arm64
-sed -i -e '(/etc/torrserv/TorrServer-linux-arm64 -p 8090 >/dev/null 2>&1 ) &' /etc/rc.local
+sed -i -e '$i (/etc/torrserv/TorrServer-linux-arm64 -p 8090 >/dev/null 2>&1 ) &' /etc/rc.local
