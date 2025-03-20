a05863b013164d39bb2dee91a0c542e32d60152e
ya158 <koros@ya.ru>
2025-03-20 14:31:41 +0300

    Update All.sh

--- All.sh
+++ All.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-wget --no-check-certificate -O /tmp/Terminal.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/Terminal.sh && chmod +x /tmp/Terminal.sh && /tmp/Terminal.sh
-rm -f /tmp/Terminal.sh
-wget --no-check-certificate -O /tmp/led.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/led.sh && chmod +x /tmp/led.sh && /tmp/led.sh
-rm -f /tmp/led.sh
-wget --no-check-certificate -O /tmp/NetBird.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/NetBird.sh && chmod +x /tmp/NetBird.sh && /tmp/NetBird.sh
-rm -f /tmp/NetBird.sh
+wget --no-check-certificate -O /tmp/Terminal.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/Terminal.sh && chmod +x /tmp/Terminal.sh && /tmp/Terminal.sh && rm -f /tmp/Terminal.sh
+wget --no-check-certificate -O /tmp/led.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/led.sh && chmod +x /tmp/led.sh && /tmp/led.sh && rm -f /tmp/led.sh
+wget --no-check-certificate -O /tmp/NetBird.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/NetBird.sh && chmod +x /tmp/NetBird.sh && /tmp/NetBird.sh && rm -f /tmp/NetBird.sh
+wget --no-check-certificate -O /tmp/TorrServer.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/TorrServer.sh && chmod +x /tmp/TorrServer.sh && /tmp/TorrServer.sh && rm -f /tmp/TorrServer.sh
