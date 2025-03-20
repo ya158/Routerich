e9591c7625847b8a15f4f7a93af6b453bf42daff
ya158 <koros@ya.ru>
2025-03-20 11:29:07 +0300

    Create ZeroTier.sh

---
+++ ZeroTier.sh
@@ -0,0 +1,32 @@
+uci add_list zerotier.mynet.join=<network_id>
+uci set zerotier.mynet.enabled='1'
+uci commit zerotier
+service zerotier restart
+
+# Create interface
+uci set network.ZeroTier=interface
+uci set network.ZeroTier.proto='none'
+uci set network.ZeroTier.device='ztXXXXXXXX' # Replace ztXXXXXXXX with your own ZeroTier interface name
+
+# Create zone
+uci add firewall zone
+uci set firewall.@zone[-1].name='vpn'
+uci set firewall.@zone[-1].input='ACCEPT'
+uci set firewall.@zone[-1].output='ACCEPT'
+uci set firewall.@zone[-1].forward='ACCEPT'
+uci set firewall.@zone[-1].masq='1'
+uci add_list firewall.@zone[-1].network='ZeroTier'
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src='vpn'
+uci set firewall.@forwarding[-1].dest='lan'
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src='vpn'
+uci set firewall.@forwarding[-1].dest='wan'
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src='lan'
+uci set firewall.@forwarding[-1].dest='vpn'
+
+# Commit changes
+uci commit
+
+/etc/init.d/firewall restart
