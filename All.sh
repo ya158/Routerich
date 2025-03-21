ef578005dd989e3fa8e6be777c1f994d08cda130
ya158 <koros@ya.ru>
2025-03-21 15:26:46 +0300

    Update All.sh

--- All.sh
+++ All.sh
@@ -4,5 +4,5 @@ wget --no-check-certificate -O /tmp/Terminal.sh https://raw.githubusercontent.co
 wget --no-check-certificate -O /tmp/led.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/led.sh && chmod +x /tmp/led.sh && /tmp/led.sh && rm -f /tmp/led.sh
 wget --no-check-certificate -O /tmp/NetBird.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/NetBird.sh && chmod +x /tmp/NetBird.sh && /tmp/NetBird.sh && rm -f /tmp/NetBird.sh
 wget --no-check-certificate -O /tmp/TorrServer.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/TorrServer.sh && chmod +x /tmp/TorrServer.sh && /tmp/TorrServer.sh && rm -f /tmp/TorrServer.sh
-wget --no-check-certificate -O /tmp/TorrentBot.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/TorrentBot.sh && chmod +x /tmp/TorrServer.sh && /tmp/TorrentBot.sh && rm -f /tmp/TorrentBot.sh
+wget --no-check-certificate -O /tmp/TelegramBot.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/TelegramBot.sh && chmod +x /tmp/TelegramBot.sh && /tmp/TelegramBot.sh && rm -f /tmp/TelegramBot.sh
 
