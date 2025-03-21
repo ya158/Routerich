0bd96ecd2b6223f94fd819a66f11208d9f6c3af9
ya158 <koros@ya.ru>
2025-03-21 15:09:59 +0300

    Update Terminal.sh

--- Terminal.sh
+++ Terminal.sh
@@ -2,6 +2,6 @@
 
 uci set ttyd.@ttyd[0].interface='0.0.0.0'
 uci commit ttyd
-/etc/init.d/ttyd restart
-/etc/init.d/rpcd restart
-/etc/init.d/uhttpd restart
+#/etc/init.d/ttyd restart
+#/etc/init.d/rpcd restart
+#/etc/init.d/uhttpd restart
