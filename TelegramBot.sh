26860024e507695a59657252f2048b62b0119647
ya158 <koros@ya.ru>
2025-03-21 16:24:42 +0300

    Update TelegramBot.sh

--- TelegramBot.sh
+++ TelegramBot.sh
@@ -8,3 +8,5 @@ opkg install modeminfo-telegram
 uci set telegrambot.config.bot_token='7618026740:AAFYLOlyNZ4PmI6BtXzILMrZCruiyDmPKRo'
 uci set telegrambot.config.chat_id='346314142'
 uci set telegrambot.config.enabled='1'
+
+uci commit telegrambot
