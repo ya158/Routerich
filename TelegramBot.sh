54619f4bbfc369743f2403f87be9927ec4a93139
ya158 <koros@ya.ru>
2025-03-21 15:27:12 +0300

    Update TelegramBot.sh

--- TelegramBot.sh
+++ TelegramBot.sh
@@ -2,5 +2,6 @@
 
 opkg update
 opkg install modeminfo-telegram
+opkg install luci-i18n-telegrambot-ru
 uci set telegrambot.config.bot_token='7618026740:AAFYLOlyNZ4PmI6BtXzILMrZCruiyDmPKRo'
 uci set telegrambot.config.chat_id='346314142'
