b574a89f9f285dcea5579be2bc99b01afcfabe8f
ya158 <koros@ya.ru>
2025-03-25 12:14:47 +0300

    Create minidlna.sh

---
+++ minidlna.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+opkg update
+opkg install minidlna
+opkg install luci-app-minidlna
+opkg install luci-i18n-minidlna-ru
+uci set minidlna.config.friendly_name='Routerich DLNA Server'
+uci set minidlna.config.enabled='1'
+uci set minidlna.config.root_container='B'
+uci set minidlna.config.db_dir='/mnt/sda2/minidlna'
+uci set minidlna.config.media_dir='/mnt/sda2'
+uci commit minidlna
