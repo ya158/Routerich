4bcb3378484056319a69dd4de863bc7fb7bea126
ya158 <koros@ya.ru>
2025-03-26 08:54:22 +0300

    Update All.sh

--- All.sh
+++ All.sh
@@ -3,6 +3,7 @@
 wget --no-check-certificate -O /tmp/Terminal.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/Terminal.sh && chmod +x /tmp/Terminal.sh && /tmp/Terminal.sh && rm -f /tmp/Terminal.sh
 wget --no-check-certificate -O /tmp/led.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/led.sh && chmod +x /tmp/led.sh && /tmp/led.sh && rm -f /tmp/led.sh
 wget --no-check-certificate -O /tmp/NetBird.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/NetBird.sh && chmod +x /tmp/NetBird.sh && /tmp/NetBird.sh && rm -f /tmp/NetBird.sh
+wget --no-check-certificate -O /tmp/ZeroTier.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/ZeroTier.sh && chmod +x /tmp/ZeroTier.sh && /tmp/ZeroTier.sh && rm -f /tmp/ZeroTier.sh
 wget --no-check-certificate -O /tmp/TorrServer.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/TorrServer.sh && chmod +x /tmp/TorrServer.sh && /tmp/TorrServer.sh && rm -f /tmp/TorrServer.sh
 wget --no-check-certificate -O /tmp/TelegramBot.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/TelegramBot.sh && chmod +x /tmp/TelegramBot.sh && /tmp/TelegramBot.sh && rm -f /tmp/TelegramBot.sh
 wget --no-check-certificate -O /tmp/luciYoutubeUnblock.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/luciYoutubeUnblock.sh && chmod +x /tmp/luciYoutubeUnblock.sh && /tmp/luciYoutubeUnblock.sh && rm -f /tmp/luciYoutubeUnblock.sh
