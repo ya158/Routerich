586e83a78ac05d2d0b90a5f6abe01b2b16d7e29c
ya158 <koros@ya.ru>
2025-03-27 19:55:16 +0300

    Update Transmission.sh

--- Transmission.sh
+++ Transmission.sh
@@ -8,5 +8,10 @@ opkg install transmission-web-control
 opkg install luci-app-transmission	
 opkg install luci-i18n-transmission-ru
 uci set transmission.@transmission[0].enabled="1"
+uci set transmission.@transmission[0].download_dir='/mnt/sda2/Torrent'
+uci set transmission.@transmission[0].config_dir='/etc/transmission'
+uci set transmission.@transmission[0].incomplete_dir='/mnt/transmission/incomplete'
+uci set transmission.@transmission[0].incomplete_dir_enabled='true'
+
 uci commit transmission
 service transmission restart
