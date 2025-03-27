0e52d140791bd7ac87c906a4e3d096a78820b3bc
ya158 <koros@ya.ru>
2025-03-27 15:37:27 +0300

    Update All.sh

--- All.sh
+++ All.sh
@@ -9,3 +9,4 @@ wget --no-check-certificate -O /tmp/TelegramBot.sh https://raw.githubusercontent
 wget --no-check-certificate -O /tmp/luciYoutubeUnblock.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/luciYoutubeUnblock.sh && chmod +x /tmp/luciYoutubeUnblock.sh && /tmp/luciYoutubeUnblock.sh && rm -f /tmp/luciYoutubeUnblock.sh
 wget --no-check-certificate -O /tmp/ftp.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/ftp.sh && chmod +x /tmp/ftp.sh && /tmp/ftp.sh && rm -f /tmp/ftp.sh
 wget --no-check-certificate -O /tmp/minidlna.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/minidlna.sh && chmod +x /tmp/minidlna.sh && /tmp/minidlna.sh && rm -f /tmp/minidlna.sh
+wget --no-check-certificate -O /tmp/Transmission.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/Transmission.sh && chmod +x /tmp/Transmission.sh && /tmp/Transmission.sh && rm -f /tmp/Transmission.sh
