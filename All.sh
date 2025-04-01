b27b3c9044378ad9635b21444ef07e8b94dbc2de
ya158 <koros@ya.ru>
2025-04-01 22:09:30 +0300

    Update All.sh

--- All.sh
+++ All.sh
@@ -7,6 +7,6 @@ wget --no-check-certificate -O /tmp/ZeroTier.sh https://raw.githubusercontent.co
 wget --no-check-certificate -O /tmp/TorrServer.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/TorrServer.sh && chmod +x /tmp/TorrServer.sh && /tmp/TorrServer.sh && rm -f /tmp/TorrServer.sh
 wget --no-check-certificate -O /tmp/TelegramBot.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/TelegramBot.sh && chmod +x /tmp/TelegramBot.sh && /tmp/TelegramBot.sh && rm -f /tmp/TelegramBot.sh
 wget --no-check-certificate -O /tmp/luciYoutubeUnblock.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/luciYoutubeUnblock.sh && chmod +x /tmp/luciYoutubeUnblock.sh && /tmp/luciYoutubeUnblock.sh && rm -f /tmp/luciYoutubeUnblock.sh
-wget --no-check-certificate -O /tmp/ftp.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/ftp.sh && chmod +x /tmp/ftp.sh && /tmp/ftp.sh && rm -f /tmp/ftp.sh
+# wget --no-check-certificate -O /tmp/ftp.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/ftp.sh && chmod +x /tmp/ftp.sh && /tmp/ftp.sh && rm -f /tmp/ftp.sh
 wget --no-check-certificate -O /tmp/minidlna.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/minidlna.sh && chmod +x /tmp/minidlna.sh && /tmp/minidlna.sh && rm -f /tmp/minidlna.sh
 wget --no-check-certificate -O /tmp/Transmission.sh https://raw.githubusercontent.com/ya158/Routerich/refs/heads/main/Transmission.sh && chmod +x /tmp/Transmission.sh && /tmp/Transmission.sh && rm -f /tmp/Transmission.sh
