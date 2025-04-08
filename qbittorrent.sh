df38081ad98521c5a190c26ea29cf9263898297f
ya158 <koros@ya.ru>
2025-04-08 20:27:40 +0300

    Update qbittorrent.sh

--- qbittorrent.sh
+++ qbittorrent.sh
@@ -6,6 +6,6 @@ opkg install luci-app-qbittorrent
 opkg install luci-i18n-qbittorrent-ru
 uci set qbittorrent.config.enabled='1'
 uci set qbittorrent.config.port='8080'
-uci set qbittorrent.config.profile_dir='/ext/qbittorrent'
+uci set qbittorrent.config.profile_dir='/etc/qbittorrent'
 
 uci commit qbittorrent
