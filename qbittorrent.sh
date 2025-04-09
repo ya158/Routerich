3684d1cfae5209242f76bd89d40da94e3ac712a9
ya158 <koros@ya.ru>
2025-04-09 13:26:47 +0300

    Update qbittorrent.sh

--- qbittorrent.sh
+++ qbittorrent.sh
@@ -4,9 +4,6 @@ opkg update
 opkg install qbittorrent
 opkg install luci-app-qbittorrent
 opkg install luci-i18n-qbittorrent-ru
-uci set qbittorrent.config.enabled='1'
-uci set qbittorrent.config.port='8080'
-uci set qbittorrent.config.profile_dir='/etc/qbittorrent'
 
 echo '[Application]>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
 echo 'FileLogger\Path=/etc/qbittorrent/logs>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
@@ -20,5 +17,9 @@ echo '[Preferences]>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf
 echo 'General\Locale=ru>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
 echo 'WebUI\AuthSubnetWhitelist=192.168.1.0/16, 100.97.246.30/32, 100.97.251.204/32, 100.97.252.139/32>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
 echo 'WebUI\AuthSubnetWhitelistEnabled=true>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+
+uci set qbittorrent.config.enabled='1'
+uci set qbittorrent.config.port='8080'
+uci set qbittorrent.config.profile_dir='/etc/qbittorrent'
 
 uci commit qbittorrent
