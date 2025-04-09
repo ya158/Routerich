b91cebfb6a36c6323d8ae8a044b4434b53a62246
ya158 <koros@ya.ru>
2025-04-09 13:12:45 +0300

    Update qbittorrent.sh

--- qbittorrent.sh
+++ qbittorrent.sh
@@ -8,4 +8,17 @@ uci set qbittorrent.config.enabled='1'
 uci set qbittorrent.config.port='8080'
 uci set qbittorrent.config.profile_dir='/etc/qbittorrent'
 
+echo '[Application]>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+echo 'FileLogger\Path=/etc/qbittorrent/logs>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+echo '>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+echo '[BitTorrent]>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+echo 'Session\DefaultSavePath=/mnt/sda2/Torrent>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+echo 'Session\TempPath=/mnt/sda2/Torrent>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+echo 'Session\TempPathEnabled=true>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+echo '>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+echo '[Preferences]>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+echo 'General\Locale=ru>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+echo 'WebUI\AuthSubnetWhitelist=192.168.1.0/16, 100.97.246.30/32, 100.97.251.204/32, 100.97.252.139/32>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+echo 'WebUI\AuthSubnetWhitelistEnabled=true>/etc/qbittorrent/qBittorrent/config/qBittorrent.conf 
+
 uci commit qbittorrent
